bd31aa5fdb9615721849703058b8a84cab655f91
BEERMOHAMED786 <149494944+BEERMOHAMED786@users.noreply.github.com>
2023-10-31 18:32:04 +0530

    Add files via upload

---
+++ Unit 3 - Challenge 2.py
@@ -0,0 +1,25 @@
+# Unit 3 - Challenge 2
+
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
+
+# Test the sort_students function
+students_list = [
+    Student("Alice", "A001", 3.5),
+    Student("Bob", "B002", 3.2),
+    Student("Charlie", "C003", 3.8),
+    Student("David", "D004", 3.6)
+]
+
+sorted_students = sort_students(students_list)
+
+print("Sorted Students based on CGPA:")
+for student in sorted_students:
+    print(f"{student.name} - {student.roll_number} - CGPA: {student.cgpa}")
